58b960e411abbb1a07dd21d81117c2d8426e9c97
BuildTools <unconfigured@null.spigotmc.org>
2023-09-18 15:25:08 +0200

    Editor V1

---
+++ Editor/Editor.py
@@ -0,0 +1,17 @@
+from Level import Level
+from BlocSelector import BlocSelector
+
+
+class Editor:
+
+    def __init__(self, screen):
+        self.level_name = input("Nom du niveau ?")
+        self.level = Level(screen, self)
+        self.bloc_selector = BlocSelector(screen, self)
+        self.current_material = 0
+
+
+
+    def update(self, events, dt):
+        self.level.draw_grid(events, dt)
+        self.bloc_selector.update(events)
---
+++ Editor/blocs.md
@@ -0,0 +1,8 @@
+# Liste des blocs
+
+# Identifiant:Nom:Fonctionnalité
+
+0 : Vide : Bloc vide 
+1 : Mur : Bloc de décor impossible à bouger
+2 : Spawn: Bloc où le robot apparait au dessus
+3 : Fin : Bloc à atteindre
