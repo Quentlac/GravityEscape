f823ffde608622988bcfd9fd221a55bf09344dff
quentin.lacombe <quentin.lacombe@microoled.net>
2023-09-19 14:12:38 +0200

    Fix shoot system

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from Item.Item2D import Item2D
 from Bloc.GravityBloc import *
 from Bloc.StaticBloc import *
+from Item.BulletItem import *
 from Player import Player
 from Level import Level
 
@@ -21,17 +22,16 @@ list_bloc = []
 
 level = Level("level", window)
 
+bullets = []
+
 def shoot(x, y):
+    dx = x - player.getPosX()
+    dy = y - player.getPosY()
 
-    s = Item2D((x, y), (5, 5))
+    visee = pygame.Vector2(dx, dy)
+    visee = visee.normalize()
 
-    for b in list_bloc:
-        if(b.testCollisionWithOtherItem(s)):
-            print("Invert", b)
-            b.invertGravity()
-            return True
-
-    return False
+    bullets.append(BulletItem((player.getPosX(), player.getPosY()), (visee.x, visee.y)))
 
 inGame = True
 
@@ -43,8 +43,7 @@ while inGame:
             inGame = False
         if event.type == MOUSEBUTTONDOWN:
             posX, posY = pygame.mouse.get_pos()
-            if not shoot(posX, posY):
-                list_bloc.append(GravityBloc((posX, posY)))
+            shoot(posX, posY)
 
     window.fill((0, 0, 0))
 
@@ -54,6 +53,10 @@ while inGame:
     level.update(dt)
 
     for b in list_bloc:
+        b.display(window)
+        b.move(dt)
+
+    for b in bullets:
         b.display(window)
         b.move(dt)
 
