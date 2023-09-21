f39854d2da255419550f42614e280e661af32669
quentin.lacombe <quentin.lacombe@microoled.net>
2023-09-21 16:40:08 +0200

    Fix bullets

--- Item/BulletItem.py
+++ Item/BulletItem.py
@@ -53,7 +53,6 @@ class BulletItem(Item2D):
 
             for b in GravityItem.getItems():
                 if(not isinstance(b, NoKillBloc) and not b._isPlayer and self.active and self.testCollisionWithOtherItem(b, dx, dy)):
-                    col = True
                     if(isinstance(b, GravityBloc)):
                         b.invertGravity(self.button)
                         self.active = False
